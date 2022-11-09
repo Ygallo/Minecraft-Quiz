d53477e40ebc6d41feb83f12eaf106e2fad2cc20
Ygallo <yaninagallo@gmail.com>
2022-11-09 18:31:57 +0000

    Add structure for game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 
-function questions() {
+function displayQuestions() {
     let radioA = document.getElementById("radioA");
     radioA.value ="this is radio A";
     console.log(radioA)
 }
 questions();
 
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+
+}
+
+function totalScore() {
+
+}
