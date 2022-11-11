836b7070589f8b0a77f26307c75f46e12abc5e72
Ygallo <yaninagallo@gmail.com>
2022-11-11 18:55:28 +0000

    Add funtion for display Question and cheking the answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,114 +1,148 @@
 /**
- * Create quiz questions
+ * Array of object with the quiz Questions
  */
 
-let questions = [
-{
-    question: 'In what year was Minecraft fully released?',
-    answerA: 2012,
-    answerB:2011,
-    answerC:2007,
-    answerD: 2010,
-    correct: 'answerB',
-}, {
-    question: 'How many players can play together, at the same time, on Minecraft Realms?', 
-    answerA: 12,
-    answerB: 8,
-    answerC: 11,
-    answerD: 10,
-    correct: 'answerD',
-}, {
-    question: 'The game follows a day and night cycle. How long does one full cycle last in real-time?',
-    answerA: '20 minutes',
-    answerB: '30 minutes',
-    answerC: '60 minutes',
-    answerD: '10 minutes',
-    correct: 'answerA',
-}, {
-    question: 'Which of the following cannot be tamed?',
-    answerA: 'wolf',
-    answerB: 'ocelot',
-    answerC: 'chicken',
-    answerD: 'horse',
-    correct: 'answerC',
-},/* {
-    question: 'What item do you need to make all of the potions?',
-    answerA: 'Water bottle',
-    answerB:
-    answerC:
-    answerD: 
-    correct: 'answerA',
-},{
-    question: 'Which species began due to a coding error?',
-    answerA: 
-    answerB:
-    answerC:
-    answerD: 'Creeper ',
-    correct: 'answerD',
-},{
-    question: 'What must you use to mine ores and stones?',
-    answerA:  ' Pickaxe',
-    answerB:
-    answerC:
-    answerD: 
-    correct: 'answerA'
-},{
-    question: 'To make one iron ingot, how many slabs of iron ore do you need?',
-    answerA: 2,
-    answerB: 0,
-    answerC: 1,
-    answerD: 5,
-    correct: 'answerC'
-},{
-    question: 'Axolotl can come in how many different colors?',
-    answerA: 5,
-    answerB: 6,
-    answerC: 2,
-    answerD: 7,
-    correct: 'answerA,
-},{
-    question: 
-    answerA: 
-    answerB:
-    answerC:
-    answerD: 
-    correct: 
-}*/
-];
+ let quizQuestions = [
+    {
+        question: 'In what year was Minecraft fully released?',
+        answerA: 2012,
+        answerB:2011,
+        answerC:2007,
+        answerD: 2010,
+        correct: 1,
+    }, {
+        question: 'How many players can play together, at the same time, on Minecraft Realms?', 
+        answerA: 12,
+        answerB: 8,
+        answerC: 11,
+        answerD: 10,
+        correct: 3,
+    }, {
+        question: 'The game follows a day and night cycle. How long does one full cycle last in real-time?',
+        answerA: '20 minutes',
+        answerB: '30 minutes',
+        answerC: '60 minutes',
+        answerD: '10 minutes',
+        correct: 0,
+    }, {
+        question: 'Which of the following cannot be tamed?',
+        answerA: 'wolf',
+        answerB: 'ocelot',
+        answerC: 'chicken',
+        answerD: 'horse',
+        correct: 2,
+    },/* {
+        question: 'What item do you need to make all of the potions?',
+        answerA: 'Water bottle',
+        answerB:
+        answerC:
+        answerD: 
+        correct: 'answerA',
+    },{
+        question: 'Which species began due to a coding error?',
+        answerA: 
+        answerB:
+        answerC:
+        answerD: 'Creeper ',
+        correct: 'answerD',
+    },{
+        question: 'What must you use to mine ores and stones?',
+        answerA:  ' Pickaxe',
+        answerB:
+        answerC:
+        answerD: 
+        correct: 'answerA'
+    },{
+        question: 'To make one iron ingot, how many slabs of iron ore do you need?',
+        answerA: 2,
+        answerB: 0,
+        answerC: 1,
+        answerD: 5,
+        correct: 'answerC'
+    },{
+        question: 'Axolotl can come in how many different colors?',
+        answerA: 5,
+        answerB: 6,
+        answerC: 2,
+        answerD: 7,
+        correct: 'answerA,
+    },{
+        question: 
+        answerA: 
+        answerB:
+        answerC:
+        answerD: 
+        correct: 
+    }*/
+    ];
+
+let index = 0;    // Current question index
 
 /**
- * Loads the questions on the page and loops through them
+ * Loads the quizQuestions on the page and loops through them
  * as the user aswers them
  */
-function displayQuestions() {
+ function displayQuestions() {
+    
+    let question = document.getElementById("question");
+    question.innerHTML = quizQuestions[index].question;
+
     let radioA = document.getElementById("radioA");
-    radioA.value = "algo";
+    radioA.value = quizQuestions[index].answerA;
     let labelA = document.getElementById('labelA');
-    labelA.innerHTML  = 'New Text';
+    labelA.innerHTML  = quizQuestions[index].answerA;
 
     let radioB = document.getElementById("radioB");
-    radioB.value = "algoB";
+    radioB.value = quizQuestions[index].answerB;
     let labelB = document.getElementById('labelB');
-    labelB.innerHTML  = 'New TextB';
+    labelB.innerHTML  = quizQuestions[index].answerB;
 
     let radioC = document.getElementById("radioC");
-    radioC.value = "algoC";
+    radioC.value = quizQuestions[index].answerC;
     let labelC = document.getElementById('labelC');
-    labelC.innerHTML  = 'New TextC';
+    labelC.innerHTML  = quizQuestions[index].answerC;
 
     let radioD = document.getElementById("radioD");
-    radioD.value = "algoD";
+    radioD.value = quizQuestions[index].answerD;
     let labelD = document.getElementById('labelD');
-    labelD.innerHTML  = 'New TextD';
+    labelD.innerHTML  = quizQuestions[index].answerD;
 
     console.log(radioA.value);
     console.log(radioB.value);
     console.log(radioC.value);
-    console.log(radioD.value)
+    console.log(radioD.value);
+
+  
 }
 displayQuestions();
 
 function checkAnswer() {
+    let selected;
+    let radioSelected= document.getElementsByName("radio1");
+     
+    for( let i=0; i< radioSelected.length;i++)
+    {
+        if(radioSelected[i].checked){
+            selected=i;
+        }
+    }
+
+    if(quizQuestions[index].correct === selected){
+        // hacer lo correta
+        alert('correct');
+        incrementScore()
+    }else{
+        alert('incorrect');
+        incrementWrongAnswer();
+    }
+    //check if we have question left
+    if(index<quizQuestions.length -1){
+        index++;
+        displayQuestions();
+    }else{
+        //no more question display game over
+        index=0;
+    }
 
 }
 
@@ -116,12 +150,29 @@ function calculateCorrectAnswer() {
     
 }
 
+/**
+ * Gets current score from the DOM and increments by 1
+ */
+
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('correct').innerText);
+    document.getElementById('correct').innerText = ++oldScore;
+}
+/**
+ * Gets current incorrect score from the DOM and increments by 1
+ */
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 
 function totalScore() {
 
 }
+
+
+
 
 
